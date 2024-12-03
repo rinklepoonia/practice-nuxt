9f25f3a087d827f0010e53652a499fe53b7375f2
rinklepoonia <rinklepoonia889@gmail.com>
2024-12-03 09:24:43 +0530

    first

---
+++ pages/about/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div>
+   <!-- <HomepageHome/> -->
+    <!-- <p class="text-red-700" style="color: red;">about</p>
+     <p class="text-red-700" style="color: red;">about</p> -->
+   <AboutpageAbout/>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+   <HomepageHome/>
+   <!-- <AboutpageAbout/> -->
+  </div>
+</template>
